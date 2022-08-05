bac27d2cd39006087708514d99cb6881511ea46c
Minsuk Kim <msk7316@gmail.com>
2022-08-05 15:37:56 +0900

    커멘트 커밋

--- src/pages/DiaryComment.jsx
+++ src/pages/DiaryComment.jsx
@@ -1,6 +1,29 @@
+import React from "react";
+
 function Diary_writeAll() {
-    return <div>일기 댓글 페이지입니다.</div>
-}
+
+    const addComent = (e)=>{
+        
+    }
+
+    return (
+        <div>
+            <span>일기 댓글 페이지입니다.</span><br/>
+            <br/>
+            <input placeholder="이름(5자 이내)" /> 
+            <input placeholder="댓글을 추가하세요.(100자 이내)" />
+            <button onClick={addComent}>추가하기</button>
+            <br/>
+            <br/>
+            <div>
+                <span>제목</span><br/>
+                <span>내용을 넣어주세요</span>
+                <button>수정</button>
+                <button>삭제</button>
+            </div>6
+        </div>
+    );
+};
 
 
 export default  Diary_writeAll
