ce803ab852161dd6eb7a3d6da34e58304833e07f
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-07 20:16:33 +0900

    버튼, 수정input 클릭이벤트

--- src/pages/DiaryDetail.jsx
+++ src/pages/DiaryDetail.jsx
@@ -9,6 +9,15 @@ function Diary_writeAll() {
     console.log(userList)
     const [edited, setEdited] = useState(false);
 
+    const onClickEditButton = () => {
+        setEdited(true);
+      };
+
+    const onClickSubmitButton = () => {
+        setEdited(false);
+    };
+
+      
     return (
         <div>
             <div>
@@ -19,11 +28,35 @@ function Diary_writeAll() {
                                 <Link to="/diarywriteall">뒤로가기</Link>
                                 <p>{user.id}</p>
                                 <p>{user.title}</p>
-                                <p>{user.contents}</p>
-                                {/* (<button 
-                                    type="button"
-                                    onClick={()=>{onClickEditBtn}}>수정</button>) */}
-                                <button>저장</button>
+                                {edited ? (
+                                        <>
+                                            <input
+                                                // name="body"
+                                                // rows="10"
+                                                // maxLength={200}
+                                                // value={updatedTodo}
+                                                // onChange={(event) => {
+                                                // setUpdatedTodo(event.target.value);
+                                                // }}
+                                            />
+                                        </>
+                                    ) : (
+                                        <p size="18">{user?.contents}</p>
+                                    )
+                                }
+
+                                {edited ? (
+                                    <button 
+                                    type='button'
+                                    onClick={onClickSubmitButton}
+                                    >저장</button>
+                                ) : (
+                                    <button 
+                                    type='button'
+                                    onClick={onClickEditButton}
+                                    >수정</button>
+                                )}
+                                
                             </div>
                         </>
                     )
