8926e6ccb11c9509df4121c49b9b72e436356d58
cordplace <msk7316@gmail.com>
2022-08-08 10:58:36 +0900

    유효성 검사 진행중

--- src/pages/DiaryComment.jsx
+++ src/pages/DiaryComment.jsx
@@ -7,22 +7,36 @@ import Button from "../components/Button";
 
 function Diary_writeAll() {
 
+    const dispatch = useDispatch();
     const comments = useSelector((state)=> state.userReducer)
     const [title, setTitle] = useState("");
     const [content, setContent] = useState(""); 
-    const dispatch = useDispatch();
     const [newText, setNewText] = useState("")
+    const [isTitle, setIsTitle] = useState(false)
 
     const RandomNum = Math.floor(Math.random() * 100);
-    
-    
 
+    const onChangeTitle = (e) =>{
+        const useTitle = e.target.value
+        setTitle(useTitle);
+        if(useTitle.length > 5){
+            alert("제목은 2글자 이상 5글자 이하로 입력해주세요!");
+            setIsTitle(isTitle)
+        }else{
+            setIsTitle(true)
+        }
+        };
+
+
+    
+    
+    // (e)=>{setTitle(e.target.value)}
 
     return (
         <div>
         <span>일기 댓글 페이지입니다.</span><br/>
         <br/>
-        <input placeholder="이름(5자 이내)" maxLength={5} type="text"  onChange={(e)=>{setTitle(e.target.value)}} /> 
+        <input placeholder="이름(5자 이내)" maxLength={5} type="text"  onChange={onChangeTitle} /> 
         <input placeholder="댓글을 추가하세요.(100자 이내)" maxLength={100} type="text"  onChange={(e)=> {setContent(e.target.value)}} />
         <Button label="추가하기" onClick={()=>{dispatch(addComment({id: RandomNum, title, content}))}}/>
         <br/>
@@ -42,7 +56,7 @@ function Diary_writeAll() {
         </div>
     </div>
     );
-}
+};
 
 
 export default  Diary_writeAll
